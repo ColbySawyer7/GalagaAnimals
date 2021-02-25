5988248b9b7bb3ebb41bc720dfe3186fad1d7e80
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-02-25 09:20:05 -0500

    Manual Merge of branch projectile-and-bakground-update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,34 +7,16 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 20.0f;
-    public GameObject projectilePrefab;
-
-    public float scoreIncrease = 100;
-    private float highScore;
-    private float score;
-    private float startDelay = 2;
-    private float scoreInterval = 1.0f;
-
-    //Health Tracking
-    public float health = 100;
-    public float foodHealth = 10;
-    public float animalDamage = 25;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("CountScore", startDelay, scoreInterval);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-         if(health <= 0){
-            //END GAME CONDTIONS
-            Debug.Log("GAME OVER!!  Final Score:" + score);
-            Destroy(gameObject);
-        }
-        
         horizontalInput = Input.GetAxis("Horizontal");
 
         if(transform.position.x < -xRange){
@@ -43,29 +25,5 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-
-        if(Input.GetKeyDown(KeyCode.Space)){
-            //Launch projective from the player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
-        
-        
-    }
-    private void OnTriggerEnter(Collider other){
-        Debug.Log(other.tag);
-        if(other.tag == "animal"){
-            health = health - animalDamage;
-            Debug.Log("Health: " + health);
-        } else if(other.tag == "food"){
-            if(!(health >= 100)){
-                health = health + foodHealth;
-                Debug.Log("Health: " + health);
-            } 
-        }    
-    }
-
-    void CountScore(){
-        score = score + scoreIncrease;
-        Debug.Log("Score:" + score);
     }
 }
