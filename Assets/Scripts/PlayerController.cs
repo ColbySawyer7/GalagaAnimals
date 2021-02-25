6217d6c903be554bb12b0996c028065db11dafd9
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-02-25 09:41:10 -0500

    Cleaned up a few errors missed from the manual merge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,33 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 20.0f;
+    //public GameObject projectilePrefab;
+
+    public float scoreIncrease = 100;
+    private float highScore;
+    private float score;
+    private float startDelay = 2;
+    private float scoreInterval = 1.0f;
+
+    //Health Tracking
+    public float health = 100;
+    public float foodHealth = 10;
+    public float animalDamage = 25;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("CountScore", startDelay, scoreInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
+         if(health <= 0){
+            //END GAME CONDTIONS
+            Debug.Log("GAME OVER!!  Final Score:" + score);
+            Destroy(gameObject);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
 
         if(transform.position.x < -xRange){
@@ -25,5 +42,29 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+    }
+
+     private void OnTriggerEnter(Collider other){
+        //Debug.Log(other.tag);
+        if(other.tag == "animal"){
+            if(health > animalDamage){
+                health = health - animalDamage;
+                Debug.Log("Health: " + health);
+            } else{
+                health = 0;
+            }
+            
+        } else if(other.tag == "food"){
+            if(health <= 100 - foodHealth){
+                health = health + foodHealth;
+                Debug.Log("Health: " + health);
+            } 
+        }    
+    }
+
+    void CountScore(){
+        score = score + scoreIncrease;
+        //Debug.Log("Score:" + score);
     }
 }
